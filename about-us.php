db23eac87637100d94a05cc971db718cdfda5674
kenguruka grevisse <kenguruka24@gmail.com>
2018-05-14 10:25:09 +0300

    added about us template

--- about-us.php
+++ about-us.php
@@ -1,27 +1,9 @@
 <?php
 
-/**
+/*
 
- * The template for displaying all pages.
+ Template Name: About Us
 
- *
-
- * This is the template that displays all pages by default.
-
- * Please note that this is the WordPress construct of pages
-
- * and that other 'pages' on your WordPress site will use a
-
- * different template.
-
- *
-
- * @package ThemeGrill
-
- * @subpackage ColorNews
-
- * @since ColorNews 1.0
- * 
  */
 
 get_header(); ?>
