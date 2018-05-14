e0dd94192a8db60d51ddd54afccfba34e181b370
kenguruka grevisse <kenguruka24@gmail.com>
2018-05-14 06:02:43 +0300

    added polylang stuff

--- functions.php
+++ functions.php
@@ -54,4 +54,19 @@ if ( ! function_exists( 'color_child_footer_copyright' ) ) :
 	}
 	endif;
 	
-	add_action( 'color_child_footer_copyright', 'color_child_footer_copyright', 10 );
+  add_action( 'color_child_footer_copyright', 'color_child_footer_copyright', 10 );
+  
+  // Polylang stuff
+  
+  add_filter( 'pll_get_post_types', 'add_cpt_to_pll', 10, 2 );
+
+  function add_cpt_to_pll( $post_types, $is_settings ) {
+      if ( $is_settings ) {
+          // hides 'my_cpt' from the list of custom post types in Polylang settings
+          unset( $post_types['my_cpt'] );
+      } else {
+          // enables language and translation management for 'my_cpt'
+          $post_types['my_cpt'] = 'my_cpt';
+      }
+      return $post_types;
+  }
